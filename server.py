e4bc414f67646c8d390b5fa3a67b917f8e77dce9
Kai Williams <154692413+ChiWilliams@users.noreply.github.com>
2024-11-25 17:46:41 -0500

    Trying to host very very simple HTML. Let's see if it works

--- server.py
+++ server.py
@@ -1,17 +1,36 @@
 from datetime import datetime
 
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import HTMLResponse
 
 import uvicorn
 
 app = FastAPI()
 
+# app.mount("/static", StaticFiles(directory = "static"), name="static")
 
-@app.get("/")
-async def hello_world():
-    print("new web request")
-    message = f"hello from disco with fastAPI!!! the datetime is {datetime.now()}"
-    return {"message": message}
+@app.get("/", response_class=HTMLResponse)
+async def home():
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Simple FastAPI Site</title>
+    </head>
+    <body>
+        <h1>This is my test website please please please work!</h1>
+    </body>
+    </html>
+"""
+
+
+
+# @app.get("/")
+# async def hello_world():
+#     print("new web request")
+#     message = f"hello from disco with fastAPI!!! the datetime is {datetime.now()}"
+#     return {"message": message}
 
 
 # if __name__ == "__main__":
