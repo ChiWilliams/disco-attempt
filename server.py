3d7965303ed4df847d45244c960f7e3774305bcd
Kai Williams <154692413+ChiWilliams@users.noreply.github.com>
2024-11-26 11:05:55 -0500

    Cleaned up example code

--- server.py
+++ server.py
@@ -8,25 +8,11 @@ import uvicorn
 
 app = FastAPI()
 
-# app.mount("/static", StaticFiles(directory = "static"), name="static")
-
 @app.get("/", response_class=HTMLResponse)
 async def home():
     with open("static/index.html", "r") as f:
         html_content = f.read()
     return html_content
-
-
-
-# @app.get("/")
-# async def hello_world():
-#     print("new web request")
-#     message = f"hello from disco with fastAPI!!! the datetime is {datetime.now()}"
-#     return {"message": message}
-
-
-# if __name__ == "__main__":
-#     app.run(host="0.0.0.0", port=8080, debug=True)
 
 if __name__ == "__main__":
     uvicorn.run("server:app", host="0.0.0.0", port=8080, reload=True)
